b95e5fccdc018e94062448f48921e031150e8ef0
JOHN SAN DIEGO <johnpatrick415@gmail.com>
2016-10-08 23:33:36 -0400

    Books lexer
    written in c

--- Lexical_Analyzer_JSD.rb
+++ Lexical_Analyzer_JSD.rb
@@ -1,16 +1,75 @@
 #!/usr/bin/ruby -w
 
-puts "This is a Lexical Analyzer"
-puts "This program will act as a interpreter for strings in a file"
+#global variables
+$charClass = 0
+$lexemme = [100]
+$nextChar = ''
+$lexLen = 0
+$token = 0
+$nextToken = 0
+
+#Character Classes
+$LETTER = 0
+$DIGIT = 1
+$UNKNOWN = 99
+
+#Token Codes
+$INT_LIT = 10
+$IDENT = 11
+$ASSIGN_OP = 20
+$ADD_OP = 21
+$SUB_OP = 22
+$MULT_OP = 23
+$DIV_OP = 24
+$LEFT_PAREN = 25
+$RIGHT_PAREN = 26
+
+$lexemme[0] = 45
+
+puts $lexemme[0]
 
 filename = ARGV.first
-
 txt = open(filename)
 
-puts "Here's your file #{filename}"
+puts "Here's your file #{filename}:"
 #print txt.read
 
-word = txt.read
-puts word
+	
+file = txt.read
 
-word.
+puts "file has #{file}"
+token1 = file.split("")
+
+def getChar()
+	nextChar = token1
+	if nextChar != EOF
+		if letter?(nextChar)
+			charClass = LETTER
+		elsif numeric?(nextChar)
+			charClass = DIGIT
+		else charClass = UNKNOWN
+		end
+		else
+		charClass = EOF
+	end
+end
+def addChar()
+	
+end
+def getNoBlanks()
+
+end
+def lex()
+
+end
+def lookUp()
+
+end
+def numeric?(lookAhead)
+  lookAhead =~ /[[:digit:]]/
+end
+def letter?(lookAhead)
+  lookAhead =~ /[[:alpha:]]/
+end
+
+	
